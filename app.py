d62f84f70cda91d1d4117a8f064b64c15cb34bcb
Kumar Saurav <athena.saurav@gmail.com>
2023-04-27 18:22:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = 'your-api-key'
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 app = Flask(__name__)
 
